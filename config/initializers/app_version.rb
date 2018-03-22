e0861f122f89c11c0bd8a40d85414659a716269f
Rafael Biriba <biribarj@gmail.com>
2018-03-22 13:17:41 -0300

    Bumping version 1.2.3

--- config/initializers/app_version.rb
+++ config/initializers/app_version.rb
@@ -1,3 +1,3 @@
-module APP; VERSION = ''; end
+module APP; VERSION = '1.2.3'; end
 # This file is created automatically by auto_tag_version gem
 # Documentation at https://github.com/rafaelbiriba/auto_tag_version
